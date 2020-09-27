4d4e14959422dc64a91bb0d43af74ed6b3a055d4
HatimDalal <dalalhatim16@gmail.com>
2020-09-27 22:02:35 +0530

    addvendorupdate

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<?php 
+<!--?php 
     include_once 'includes/connectdb.php';
-?>
-
+?--> 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,11 +13,13 @@
         <header>
             <nav id="main-navigation">
                 <ul>
-                    <li><a href="index.php">Vendors List</a></li>
+                    <!--<li><a href="index.php">Vendors List</a></li> -->
                     <li><a href="repositorydisplay.php">Registory</a></li>
                     <li><a href="printbill.php">Print bill</a></li>
                     <li><a href="addproduct.php">Add new product</a></li>
                     <li><a href="includes/connectdb.php">db</a></li>
+                    <li><a href="addvendor.php">new vendor</a></li>
+                    <li><a href="login.php">Login</a></li>
                 </ul>
             </nav>
         </header>
---
+++ login.php
@@ -0,0 +1,58 @@
+<!--form method="post" action="" name="signin-form">
+    <div class="form-element">
+        <label>Username</label>
+        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+    </div>
+    <div class="form-element">
+        <label>Password</label>
+        <input type="password" name="password" required />
+    </div>
+    <button type="submit" name="login" value="login">Log In</button>
+</form-->
+
+<?php 
+    include_once 'includes/connectdb.php'; 
+
+if(isset($_POST['username'])){
+    
+    $Uname=$_POST['username'];
+    $Password=$_POST['password'];
+    
+    $sql="select * from users where Uname='".$Uname."'AND Password='".$Password."'";
+    
+    $result=mysql_query($sql);
+    
+    if(mysql_num_rows($result)==1){
+        echo " You Have Successfully Logged in";
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    }
+        
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Login Form in HTML5 and CSS3</title>
+	<!--link rel="stylesheet" a href="css\style.css">
+	<link rel="stylesheet" a href="css\font-awesome.min.css"-->
+</head>
+<body>
+	<div class="container">
+		<form method="POST" action="">
+			<div class="form-input">
+				<input type="text" name="text" placeholder="Enter the User Name"/>	
+			</div>
+			<div class="form-input">
+				<input type="password" name="password" placeholder="password"/>
+			</div>
+			<input type="submit" type="submit" value="LOGIN" class="btn-login"/>
+		</form>
+	</div>
+</body>
+</html>
